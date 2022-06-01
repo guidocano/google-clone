7a54af161393de6c5053006685b73483a82ff2d4
Guido <guido.calciano@hotmail.com>
2022-06-01 15:46:14 -0300

    minor fixes

--- src/api/useGoogleSearch.js
+++ src/api/useGoogleSearch.js
@@ -1,13 +1,14 @@
 import {useState, useEffect} from 'react'
 
-const CONTEXT_KEY = "efdab000014cfaabf"
-const API_KEY = "AIzaSyCvB-j8tvOgg5Mfj45PU9J0-1-gh9Ak8ZA"
 
+// some use
+// const CONTEXT_KEY = "efdab000014cfaabf"
+// const API_KEY = "AIzaSyCvB-j8tvOgg5Mfj45PU9J0-1-gh9Ak8ZA"
 
+const CONTEXT_KEY = "eeaf17c9da42d2505"
+const API_KEY = "AIzaSyB4XHUk1yAYe8z1OpJvXRP25kvAF4K_UD4"
 
-// const CONTEXT_KEY = "eeaf17c9da42d2505"
-// const API_KEY = "AIzaSyB4XHUk1yAYe8z1OpJvXRP25kvAF4K_UD4"
-
+// depleted
 // const CONTEXT_KEY = "efdab000014cfaabf"
 // const API_KEY = "AIzaSyBCViAH0RIky7A6iThcbJeTzjC7dOKf3ps"
 
