233848a3fe85009a6463e915e49c589d9b373a6c
Guido <guido.calciano@hotmail.com>
2022-06-08 18:27:24 -0300

    started dark mode

--- src/api/TermContext.js
+++ src/api/TermContext.js
@@ -6,9 +6,15 @@ export const TermContext = createContext();
 export const TermProvider = (props) => {
     const [input, setInput] = useState("")
     const [activeInput, setActiveInput] = useState("")
+    const [theme, setTheme] = useState("light")
 
     return (
-        <TermContext.Provider value={{input: [input, setInput], active: [activeInput, setActiveInput]}}>
+        <TermContext.Provider 
+            value={{
+                input: [input, setInput], 
+                active: [activeInput, setActiveInput], 
+                themes: [theme, setTheme]
+                }}>
             {props.children}
         </TermContext.Provider>
     )
--- src/components/NavbarRight.js
+++ src/components/NavbarRight.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useContext} from "react";
 import AppsIcon from '@mui/icons-material/Apps';
 import "../pages/SearchPage.css"
 import ButtonBlue from "../components/ButtonBlue";
 import Switch from '@mui/material/Switch';
+import {TermContext} from "../api/TermContext"
 
 
 function NavbarRight() {
+
+  const {themes} = useContext(TermContext)
+  const [theme, setTheme] = themes
+
+  console.log(theme)
+
+  const switchHandler = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
   return (
     <div className="right-navbar">
-      <Switch label="Dark Mode" color="info" />
+      <Switch 
+        label="Dark Mode" 
+        color="info" 
+        onChange={switchHandler}
+        checked={theme === "light"}
+        />
       <div><AppsIcon className="apps-icon"/></div>
       <div><ButtonBlue text="Sign in"/></div>
     </div>
